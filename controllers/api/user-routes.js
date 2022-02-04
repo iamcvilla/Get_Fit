513f87d34f613249cd49c52e600d8c8541b11acf
KRD13 <rauchdavis13@gmail.com>
2022-02-03 22:03:52 -0500

    forgot some more files

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
-const { User, Post, Comment, Vote } = require('../../models');
-const withAuth = require('../utils/auth');
+const { User, Post, Excercise, Vote } = require('../../models');
+
 
 // get all users
 router.get('/', (req, res) => {
@@ -22,7 +22,7 @@ router.get('/:id', (req, res) => {
     },
     include: [
       {
-        model: Post,
+        model: Excercise,
         attributes: ['id', 'title', 'post_url', 'created_at']
       },
       {
