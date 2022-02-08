a0314a6c4befb7dce551a049cdf3e1e3b43db624
KRD13 <rauchdavis13@gmail.com>
2022-02-07 19:35:14 -0500

    added handlebar, routes and javascript files for stats (graphs)

--- controllers/index.js
+++ controllers/index.js
@@ -4,9 +4,11 @@ const router = require('express').Router();
 const apiRoutes = require('./api/');
 const homeRoutes = require('./home-routes.js');
 const dashboardRoutes = require('./dashboard-routes.js');
+const statsRoutes = require('./stats-routes');
 
 router.use('/', homeRoutes);
 router.use('/dashboard', dashboardRoutes);
+router.use('/stats', statsRoutes);
 router.use('/api', apiRoutes);
 
 module.exports = router;
